91e8d27183704e56c45dff4363124b804b5f7533
JollyGolf <kybcemwesley@gmail.com>
2017-09-03 14:31:28 +0300

    fix previous commit

--- categories/Body_Parts/b_modal-type-test.php
+++ categories/Body_Parts/b_modal-type-test.php
@@ -8,28 +8,28 @@
       <div class="modal-body">
         <div class="panel-group">
 
-		  <a href="b_FI-OW.php" class="a-none">
+		  <a href="categories/Body_Parts/b_FI-OW.php" class="a-none">
             <div class="panel panel-danger cursor-pointer hover-border">
       	      <div class="panel-heading"><span class="fa fa-connectdevelop fa-spin fa-2x">&nbsp;&nbsp;</span>Four Image - One Word</div>
       	      <div class="panel-body">Choose a picture that matches the word.</div>
       	    </div>
           </a>
 
-          <a href="b_OI-FW.php" class="a-none">
+          <a href="categories/Body_Parts/b_OI-FW.php" class="a-none">
       	    <div class="panel panel-info cursor-pointer hover-border">
       	      <div class="panel-heading"><span class="fa fa-connectdevelop fa-spin fa-2x">&nbsp;&nbsp;</span>One Image - Four Word</div>
       	      <div class="panel-body">Select one of the four words that corresponds to the image.</div>
       	    </div>
 		  </a>
 
-		  <a href="b_FW-FW.php" class="a-none">
+		  <a href="categories/Body_Parts/b_FW-FW.php" class="a-none">
       	    <div class="panel panel-warning cursor-pointer hover-border">
       	      <div class="panel-heading"><span class="fa fa-connectdevelop fa-spin fa-2x">&nbsp;&nbsp;</span>Four Word - Four Word</div>
       	      <div class="panel-body">Combine the left and right sides to get the phrase.</div>
       	    </div>
 		  </a>
 
-		  <a href="b_FI-FW.php" class="a-none">
+		  <a href="categories/Body_Parts/b_FI-FW.php" class="a-none">
       	    <div class="panel panel-success cursor-pointer hover-border">
       	      <div class="panel-heading"><span class="fa fa-connectdevelop fa-spin fa-2x">&nbsp;&nbsp;</span>Four Image - Four Word</div>
       	      <div class="panel-body">Combine the image and the corresponding word.</div>
--- categories/Human_Anatomy/h_FW-FW.php
+++ categories/Human_Anatomy/h_FW-FW.php
@@ -4,7 +4,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title></title>
-  <?php require_once("parts/link-top.php"); ?>
+  <link rel="stylesheet" href="../../css/select-game.css">
+  <?php require_once("../../parts/link-top.php"); ?>
+  <link rel="stylesheet" href="../../font/font.css">
+  <link rel="stylesheet" href="../../libs/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../../css/navigation.css">
   <style>
     .checkbox-FI-FW {
       width: 15px;
@@ -17,9 +21,31 @@
 </head>
 <body>
 
-<?php require("parts/nav.php"); ?>
-<?php require("parts/modal-authentication-window.php"); ?>
-<?php require("parts/modal-registration-window.php"); ?>
+<nav class="navbar navbar-default">
+  <div class="container name-brand">
+    <div class="navbar-header">
+      <a href="../../index.php" class="navbar-brand text-success"><h3><span class="text-info">ENGLISH IN MEDICINE</span></h3></a>
+    </div>
+    <div>
+      <ul class="nav navbar-nav">
+        <li><a href="../../index.php"><h3><span class="text-primary">Home</span></h1></a></li>
+        <li><a href="../../sections.php"><h3><span class="text-primary">Sections</span></h1></a></li>
+        <li><a href="../../vocabulary.php"><h3><span class="text-primary">Vocabluary</span></h1></a></li>
+      </ul>
+      <div class="log-in-nav">
+        <a>
+          <span class="fa fa-address-book fa-3x cursor-pointer" data-toggle="modal" data-target="#modal-window-auth"></span>
+        </a>
+        <input type="text" class="btn btn-primary">
+        <a>
+          <span class="fa fa-address-book-o fa-3x fa-flip-horizontal cursor-pointer" data-toggle="modal" data-target="#modal-window-reg"></span>
+        </a>
+      </div>
+    </div>
+  </div>
+</nav>
+<?php require("../../parts/modal-authentication-window.php"); ?>
+<?php require("../../parts/modal-registration-window.php"); ?>
 
 <div class="container">
   <span id="after-append"></span>
@@ -53,7 +79,7 @@
 
 <button class="btn btn-warning btn-lg btn-block FW-FW-right btn-check"><span class="fa fa-check-circle"></span> Check</button>
 
-<?php require("parts/link-bottom.php"); ?>
+<?php require("../../parts/link-bottom.php"); ?>
 
 <script type="text/javascript">
 $('#btn-clear').on('click', function add_fw_fw(){
